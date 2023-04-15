60396825ddcd1a8faaa6c2975277712f90fc9a61
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-04-15 21:46:15 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # F3-Contest-1-April_15-2023
 Create a form using HTML,CSS and JS and execute given instruction 
+
+
+
+Tasks
+Make a simple form, execute the following event handlers.
+The form should contain 3 inputs - name, profession and age. All of these values are required.
+By default as soon as the user goes on the screen they see the default view with 0 added employees. A function is executed on click of this button which appends an object into an array.
+As they keep adding employees this array is mapped to show the data below Added Employees
+An error message is shown in red if the required fields are empty.
+A success message is shown in green if the fields are filled and the employee was added successfully.
+The structure of the array should be - [{id:1,name:"jack",profession:"developer",age:20}, {id:2,name:"john",profession:"admin",age:28}.... and so on] (The ID is given automatically)
+On clicking of the delete user button that specific object should be removed from the array and the div should disappear.
+
+
+
+
+Marking Scheme (100)
+User Interface- UI (20 Marks)
+Error Message (10 Marks)
+Success Message (15 Marks)
+Add employee function (20 Marks)
+Mapping of added employees (10 Marks)
+Deleting users (25 Marks)
+
+Figma Link :
+https://www.figma.com/file/rayp6Dv4jjCrj017Rmva5j/Adding-Employees?node-id=2-43&t=HfcLT7kU7vvTCFRb-0
---
+++ form.js
@@ -0,0 +1,121 @@
+
+
+//let arr=[{id:1,name:"jack",profession:"developer",age:20}, {id:2,name:"john",profession:"admin",age:28},{id:2,name:"smith",profession:"admin",age:28}];
+
+let arr=[];
+
+let NameEmp=document.getElementById("name");
+let AgeEmp=document.getElementById("age");
+let professionEmp=document.getElementById("Profession");
+
+ document.getElementById('table').classList.add('hidden');
+
+
+
+// labels field
+
+let sucess=document.getElementById("sucess");
+let error =document.getElementById("error");
+let noOfEmployee  = document.getElementById('zero')
+
+
+// table data
+
+let table_data=document.getElementById("tableData");
+
+
+
+ //button
+let button=document.getElementById("adduser");
+button.addEventListener("click", addEmployees);
+
+
+
+function addEmployees(){
+    let Emp_name=NameEmp.value ;
+    let Emp_age=AgeEmp.value ;
+    let Emp_prof= professionEmp.value;
+
+    // console.log(Emp_name);
+    // console.log(Emp_age);
+    // console.log(Emp_prof);
+
+    if(Emp_name && Emp_age && Emp_prof){
+
+        sucess.classList.remove("hidden") // sucess message now display on the scren
+        error.classList.add("hidden");  // now hide due to satisfy above condition
+        noOfEmployee.classList.add("hidden"); // hide .
+        document.getElementById('table').classList.remove('hidden');
+
+
+        // craate an object and assign value to it ;
+
+        let obj={
+            Name :Emp_name,
+            Age :Emp_age,
+            profession:Emp_prof
+        }
+         
+
+        // now push the obj value to an array .
+
+        arr.push(obj);
+
+        // display emplyoyee table .
+     //console.log(arr);
+
+        displayEmployeeTable();
+
+    }
+     else{
+        // if input status empty .
+          
+        sucess.classList.add("hidden") ; // sucess message now hidden
+        error.classList.add("hidden");
+        noOfEmployee.classList.remove('hidden');
+
+     }
+}
+
+
+   
+//make a function to dispalay data of table .
+    function displayEmployeeTable() {
+
+        table_data.innerHTML="";
+
+        arr.map((item,index) =>{
+             
+            
+            table_data.innerHTML += `
+            <tr>
+            <td> id: ${String(index + 1).padStart(2, '0')}.</td>
+            <td>Name: ${item.Name}</td>
+            <td>Profession: ${item.profession}</td>
+            <td>Age: ${item.Age}</td>
+            
+            <td> <button class="removebtn" onclick="deleteUserWithIndex(${index})">Delete User </button></td>
+            </tr>`      
+            
+
+        });
+     //  console.log(table_data);
+       console.log(arr);
+
+    }
+    
+    
+
+
+      
+
+
+
+
+
+
+   
+
+
+
+    
